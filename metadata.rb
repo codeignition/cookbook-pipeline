c34352b630971815f793000bc30daaf46a626d3c
Sinister Light <me@sinisterlight.com>
2013-12-04 13:08:00 +0530

    [Shobhit] adding dependencies in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ license          'All rights reserved'
 description      'Installs/Configures pipline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'docker'
+depends 'jenkins'
